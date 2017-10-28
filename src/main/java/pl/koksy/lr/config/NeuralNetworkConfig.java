5bf9828c7967ac991e641d0e494a129fe3f24e9f
Dawid Kunert <enofod@gmail.com>
2017-10-28 17:25:34 +0200

    Add valid network

--- src/main/java/pl/koksy/lr/config/NeuralNetworkConfig.java
+++ src/main/java/pl/koksy/lr/config/NeuralNetworkConfig.java
@@ -7,13 +7,13 @@ public abstract class NeuralNetworkConfig {
 
     public static final int NUMBER_OF_CHANNELS = 1;
     public static final int NUMBER_OF_LABELS = 26;
-    public static final int ITERATIONS = 10;
+    public static final int ITERATIONS = 200;
 
     public static final double LEARNING_RATE = 1e-6;
     public static final double L2_REGULARIZATION = 1e-4;
     public static final double L1_REGULARIZATION = 1e-6;
 
-    public static final int EPOCS_NUMBER = 10;
+    public static final int EPOCS_NUMBER = 5;
 
     public static final int NEURONS = 200;
 
